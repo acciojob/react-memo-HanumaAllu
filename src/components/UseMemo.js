56986387c67242017b227835fb7904c3d0e38375
Allu Hanuma Reddy <82640761+HanumaAllu@users.noreply.github.com>
2024-11-25 12:38:55 +0530

    Update UseMemo.js

--- src/components/UseMemo.js
+++ src/components/UseMemo.js
@@ -1,1 +1,19 @@
+import React, { useMemo } from 'react';
+
+const UseMemo = ({ counter }) => {
+  
+  const squareCounter = useMemo(() => {
+    console.log("Calculating square...");
+    return counter * counter;
+  }, [counter]); 
+
+  return (
+    <div>
+      <h3>UseMemo Example</h3>
+      <p>Counter square: {squareCounter}</p>
+    </div>
+  );
+};
+
+export default UseMemo;
 
